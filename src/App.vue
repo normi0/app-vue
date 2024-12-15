c3aaf24e925f06647341894b474d895e78116246
normi0 <seif23achi@gmail.com>
2024-12-15 20:25:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+  name: 'App',
+  data(){
+    return{
+      title:'My First App ;)'
+    }
+  }
+}
+</script>
+
+<template>
+<h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+#app{
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: rgb(2, 2, 70);
+  margin-top: 60px;
+}
+h1{
+display: flex;
+justify-content: center;
+align-content: center;
+border-bottom: 1px solid lightpink;
+padding-bottom:10px ;
+}
+</style>
